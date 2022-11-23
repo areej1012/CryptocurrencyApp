d08d680f07aaef42ef64f184f621c2ac3afc9a17
areej <areej101223@gmail.com>
2022-11-23 16:36:23 +0300

    AppModule

--- app/src/main/java/com/example/cryptocurrencyapp/common/Constants.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/common/Constants.kt
@@ -1,4 +1,6 @@
 package com.example.cryptocurrencyapp.common
 
 object Constants {
+
+    const val  Base_url = "https://api.coinpaprika.com/"
 }
--- app/src/main/java/com/example/cryptocurrencyapp/di/AppModule.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/di/AppModule.kt
@@ -1,7 +1,39 @@
 package com.example.cryptocurrencyapp.di
 
+import com.example.cryptocurrencyapp.common.Constants
+import com.example.cryptocurrencyapp.data.remote.CoinAPI
+import com.example.cryptocurrencyapp.data.repository.CoinRepositoryImpl
+import com.example.cryptocurrencyapp.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
 
 // it's help us to make our dependencies replaceable ex database instance
 // pr api instance
+@Module
+// that mean all the dependencies in the module live as long as app does
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    // ths for make sure there is only oa single instance throughout the whole lifetime
+    @Singleton
+    fun providePaprikaApi(): CoinAPI{
+        return Retrofit.Builder().
+                baseUrl(Constants.Base_url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinAPI::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRep(api : CoinAPI) : CoinRepository{
+        return CoinRepositoryImpl(api)
+    }
 }
