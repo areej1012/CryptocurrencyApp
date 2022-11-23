8940bf30550ebabd2d66b41e59a574fa6e49ce54
areej <areej101223@gmail.com>
2022-11-23 16:14:14 +0300

    add AppModule

---
+++ app/src/main/java/com/example/cryptocurrencyapp/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.example.cryptocurrencyapp.di
+
+
+// it's help us to make our dependencies replaceable ex database instance
+// pr api instance
+object AppModule {
+}
---
+++ app/src/main/java/com/example/cryptocurrencyapp/domain/use_case/get_coins_singular/GetCoinsDetailsUseCase.kt
@@ -0,0 +1,32 @@
+package com.example.cryptocurrencyapp.domain.use_case.get_coins_singular
+
+import com.example.cryptocurrencyapp.common.Resource
+import com.example.cryptocurrencyapp.data.remote.DTO.toCoin
+import com.example.cryptocurrencyapp.data.remote.DTO.toCoinDetail
+import com.example.cryptocurrencyapp.domain.model.Coin
+import com.example.cryptocurrencyapp.domain.model.CoinDetail
+import com.example.cryptocurrencyapp.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsDetailsUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(coinId : String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            // we want execute this use case now and get coins
+            // and should display out progress
+            emit(Resource.Loading())
+            val coins = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coins))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+        } catch (e: IOException){
+            emit(Resource.Error("Couldn't reach server, Check your internet connection"))
+
+        }
+    }
+}
